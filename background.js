5b9f6fed80bbe062ba08c535c98557a2eb8cd09b
Your Name <dannydotdev@gmail.com>
2024-06-02 18:39:31 +0100

    getting the youtube page, youtube video , it's ID and params

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  if (tab.url && tab.url.includes("youtube.com/watch")) {
+    //To make sure we are on a page that says youtube.com/watch because every youtube video has it
+    const queryParameters = tab.url.split("?")[1];
+    //Grabs all the params of a youtube video, which is ideally unique
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: "NEW",
+      videoId: urlParameters.get("v"),
+      random: "random",
+    });
+  }
+});
